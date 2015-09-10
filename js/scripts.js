300d9c74ee114b172fe9210d39994c79786c80cf
Micah Courey & Marvin Nikijuluw <student@epicodus.com>
2015-09-10 12:19:57 -0700

    refactor function to display all count by numbers and add jquery and html

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
 var countUpBy = function(count_by, count_to) {
+  var result = [count_by];
   var sum = count_by;
   while (sum <= count_to - count_by) {
-    sum += count_by;
+    var i = sum += count_by;
+    result.push(i);
   }
-  return sum;
+  return result;
 }
+
+$(document).ready(function() {
+  $("form#countupby").submit(function(event) {
+    var count_by = parseInt($("input#number1").val());
+    var count_to = parseInt($("input#number2").val());
+    var result = countUpBy(count_by, count_to);
+
+    $(".number1").text(count_by);
+    $(".number2").text(count_to);
+    $(".result").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,10 @@
 describe('countUpBy', function() {
   it("checks the count_by input and adds same number", function() {
-    expect(countUpBy(5, 10)).to.equal(10, 10);
+    expect(countUpBy(5, 10)).to.eql([5, 10]);
   })
 
   it("checks if count_by does not exceed equal to", function() {
-  expect(countUpBy(6,200)).to.equal(198);
+  expect(countUpBy(10,104)).to.eql([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]);
   })
 
 })
